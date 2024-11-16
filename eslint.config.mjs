5cf6fc26d3b47968d2c9b3db1790695eb7586321
Mitchel Hsu <mitchel101350@gmail.com>
2024-11-16 15:52:45 -0800

    feat(test): add linters and husky for pre-commit hook

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import prettierConfig from 'eslint-config-prettier';
+import pluginPrettier from 'eslint-plugin-prettier';
+import pluginHtml from 'eslint-plugin-html';
+import pluginImport from 'eslint-plugin-import';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ['**/*.js'], languageOptions: { sourceType: 'script' } },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  prettierConfig,
+  {
+    plugins: {
+      prettier: pluginPrettier,
+      html: pluginHtml,
+      import: pluginImport,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
+];
