ad5fdd8a45ec03870ef8fc762147f58f74589d8c
Rashi Bhansali <rashibhansali2000@gmail.com>
2024-11-16 14:01:26 -0800

    feat: adds playwright support to test Chrome ext

---
+++ playwright.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  projects: [
+    {
+      name: 'unit', // Project for unit tests
+      testDir: './tests', // Points to the tests folder
+      use: {
+        browserName: 'chromium', // Can be configured to use chromium for unit tests
+        headless: true,
+        args: [
+          `--disable-extensions-except=${path.resolve(__dirname)}`,
+          `--load-extension=${path.resolve(__dirname)}`
+        ]
+      }
+    },
+    {
+      name: 'e2e', // Project for E2E tests
+      testDir: './e2e', // Points to the e2e folder
+      use: {
+        browserName: 'chromium',
+        headless: false, // We need a visible browser for E2E tests
+        args: [
+          `--disable-extensions-except=${path.resolve(__dirname)}`,
+          `--load-extension=${path.resolve(__dirname)}`
+        ]
+      }
+    }
+  ]
+};
